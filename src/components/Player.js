7d5e60b555cdcb16890a5e6e4b1f8d29e5ba77cd
Mass-rj05 <krzysztof.mirek.mass@gmail.com>
2020-11-29 22:34:05 +0100

    fontawesome added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleRight,
+  faAngleLeft,
+} from "@fortawesome/free-solid-svg-icons";
 const Player = () => {
   return (
     <div className="player-container">
@@ -9,7 +14,15 @@ const Player = () => {
         <p>End Time</p>
       </div>
 
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
     </div>
   );
 };
