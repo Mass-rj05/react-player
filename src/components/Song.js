94208693ba1e9b3d4474be35bd892eb58a9e1d0f
Mass-rj05 <krzysztof.mirek.mass@gmail.com>
2020-11-29 22:24:33 +0100

    Created components

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Player Componmtent</h1>
+      <h1>Player Componmtent</h1>
+      <h1>Artist </h1>
+    </div>
+  );
+};
+
+export default Song;
